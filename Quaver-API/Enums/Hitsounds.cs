4998f2fb850f3199b86fee2824138c3fca49b089
Swan <contact@swan.moe>
2018-01-06 13:41:29 -0500

    Add: Enum for HitSounds

---
+++ Quaver-API/Enums/Hitsounds.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.API.Enums
+{
+    [Flags]
+    public enum HitSounds
+    {
+        Normal = 1 << 0, // This is 1, but Normal should be played regardless if it's 0 or 1.
+        Whistle = 1 << 1, // 2
+        Finish = 1 << 2, // 4
+        Clap = 1 << 3 // 8
+    }
+}
