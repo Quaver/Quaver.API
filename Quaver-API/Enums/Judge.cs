8b60bc33c3515358b58c7784a9f59373eebaa857
Swan <contact@swan.moe>
2018-01-31 15:34:10 -0500

    Put Judge enum in Quaver.API

---
+++ Quaver-API/Enums/Judge.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.API.Enums
+{
+    /// <summary>
+    ///     An enum containing all of our judges
+    /// </summary>
+    public enum Judge
+    {
+        Marv,
+        Perf,
+        Great,
+        Good,
+        Okay,
+        Miss
+    }
+}
