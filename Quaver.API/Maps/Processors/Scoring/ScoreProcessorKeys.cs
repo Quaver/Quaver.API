cf893f9b77d3d71cc9042a3b4ce48bfcecd1de22
Swan <me@swan.moe>
2018-05-06 21:45:46 -0400

    Create ScoreProcessorKeys derivative class

--- Quaver.API/Maps/Processors/Scoring/ScoreProcessorKeys.cs
+++ Quaver.API/Maps/Processors/Scoring/ScoreProcessorKeys.cs
@@ -1,7 +1,79 @@
-﻿namespace Quaver.API.Maps.Processors.Scoring
+﻿using System.Collections.Generic;
+using Quaver.API.Enums;
+
+namespace Quaver.API.Maps.Processors.Scoring
 {
-    public class ScoreProcessorKeys
+    public class ScoreProcessorKeys : ScoreProcessor
     {
-        
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        public override Dictionary<Judgement, int> JudgementWindow { get; } = new Dictionary<Judgement, int>
+        {
+            {Judgement.Marv, 16},
+            {Judgement.Perf, 40},
+            {Judgement.Great, 73},
+            {Judgement.Good, 103},
+            {Judgement.Okay, 127},
+            {Judgement.Miss, 164}
+        };
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        public override Dictionary<Judgement, int> JudgementScoreWeighting { get; } = new Dictionary<Judgement, int>()
+        {
+            {Judgement.Marv, 100},
+            {Judgement.Perf, 50},
+            {Judgement.Great, 25},
+            {Judgement.Good, 10},
+            {Judgement.Okay, 5},
+            {Judgement.Miss, 0}
+        };
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        public override Dictionary<Judgement, int> JudgementHealthWeighting { get; } = new Dictionary<Judgement, int>()
+        {
+            {Judgement.Marv, 100},
+            {Judgement.Perf, 50},
+            {Judgement.Great, 25},
+            {Judgement.Good, 10},
+            {Judgement.Okay, 5},
+            {Judgement.Miss, 0}
+        };
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        public override Dictionary<Grade, int> GradePercentage { get; } = new Dictionary<Grade, int>()
+        {
+            {Grade.None, -1},
+            {Grade.XX, 100},
+            {Grade.X, 100},
+            {Grade.SS, 99},
+            {Grade.S, 95},
+            {Grade.A, 90},
+            {Grade.B, 80},
+            {Grade.C, 70},
+            {Grade.D, 60},
+            {Grade.F, -1}
+        };
+
+         /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="map"></param>
+        public ScoreProcessorKeys(Qua map) : base(map)
+        {
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        public override void CalculateScoreForObject()
+        {
+        }
     }
 }
