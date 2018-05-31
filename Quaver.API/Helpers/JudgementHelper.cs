2b3bb6e272119aee77d9d894f6cb11474dbada58
Swan <me@swan.moe>
2018-05-31 16:25:48 -0400

    Add helper method to get short judge name

---
+++ Quaver.API/Helpers/JudgementHelper.cs
@@ -0,0 +1,28 @@
+﻿using Quaver.API.Enums;
+
+namespace Quaver.API.Helpers
+{
+    public class JudgementHelper
+    {
+        public static string JudgementToShortName(Judgement j)
+        {
+            switch (j)
+            {
+                case Judgement.Marvelous:
+                    return "MV";
+                case Judgement.Perfect:
+                    return "PF";
+                case Judgement.Great:
+                    return "GR";
+                case Judgement.Good:
+                    return "GD";
+                case Judgement.Okay:
+                    return "OK";
+                case Judgement.Miss:
+                    return "MS";
+            }
+
+            return "";
+        }
+    }
+}
