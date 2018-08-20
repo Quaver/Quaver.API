bba0bce96f3e7b4f5578848dc35eef28824ec1db
Swan <me@swan.moe>
2018-08-20 15:46:41 -0400

    Add helper method to convert GameMode to short hand

---
+++ Quaver.API/Helpers/ModeHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Quaver.API.Enums;
+
+namespace Quaver.API.Helpers
+{
+    public static class ModeHelper
+    {
+        /// <summary>
+        ///     Converts game mode to short hand version.
+        /// </summary>
+        /// <param name="mode"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static string ToShortHand(GameMode mode)
+        {
+            switch (mode)
+            {
+                case GameMode.Keys4:
+                    return "4K";
+                case GameMode.Keys7:
+                    return "7K";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(mode), mode, null);
+            }
+        }
+    }
+}
