30694867f0be3f8a79982680fdfb3e52c15a3c42
staravia <denysenesouma@gmail.com>
2018-09-07 09:18:15 -0700

    Update GetGradientColor

--- Quaver.API/Qss/Colors.cs
+++ Quaver.API/Qss/Colors.cs
@@ -10,32 +10,57 @@ namespace Quaver.API.Qss
     internal class Colors
     {
         /// <summary>
-        /// Color for Beginner Tier (Skill Lvl 0 - 10)
+        /// QSR required in order to be considered first tier
+        /// </summary>
+        public const float TIER_1_RATING = 10;
+
+        /// <summary>
+        /// QSR required in order to be considered second tier
+        /// </summary>
+        public const float TIER_2_RATING = 20;
+
+        /// <summary>
+        /// QSR required in order to be considered third tier
+        /// </summary>
+        public const float TIER_3_RATING = 30;
+
+        /// <summary>
+        /// QSR required in order to be considered fourth tier
+        /// </summary>
+        public const float TIER_4_RATING = 40;
+
+        /// <summary>
+        /// QSR tiers limit
+        /// </summary>
+        public const float MAX_TIER_RATING = 50;
+
+        /// <summary>
+        /// Color for Beginner Tier Songs (QSR 0 - 10)
         /// </summary>
         public static Color TierZeroColor { get; } = Color.FromArgb(128, 128, 128, 0);
 
         /// <summary>
-        /// Color for First Tier Players (Skill Lvl 10 - 20)
+        /// Color for First Tier Songs (QSR 10 - 20)
         /// </summary>
         public static Color TierOneColor { get; } = Color.FromArgb(255, 0, 255, 0);
 
         /// <summary>
-        /// Color for Second Tier Players (Skill Lvl 20 - 30)
+        /// Color for Second Tier Songs (QSR 20 - 30)
         /// </summary>
         public static Color TierTwoColor { get; } = Color.FromArgb(255, 255, 255, 0);
 
         /// <summary>
-        /// Color for Third Tier Players (Skill Lvl 30 - 40)
+        /// Color for Third Tier Songs (QSR 30 - 40)
         /// </summary>
         public static Color TierThreeColor { get; } = Color.FromArgb(255, 255, 0, 0);
 
         /// <summary>
-        /// Color for Fourth Tier Players (Skill Lvl 40+)
+        /// Color for Fourth Tier Songs (QSR 40+)
         /// </summary>
         public static Color TierFourColor { get; } = Color.FromArgb(255, 255, 0, 255);
 
         /// <summary>
-        /// Max Tier Color (Lvl 50+)
+        /// Max Tier Color (QSR 50+)
         /// </summary>
         public static Color MaxTierColor { get; } = Color.FromArgb(255, 255, 200, 255);
 
@@ -47,8 +72,52 @@ namespace Quaver.API.Qss
         /// <returns></returns>
         public static Color GetStrainRatingColor(float qsr)
         {
-            var color = Color.FromArgb(255, 255, 255, 255);
-            return color;
+            // Gradient value for two colors
+            float val;
+
+            // Tier 0 and negative rating?
+            if (qsr <= 0)
+            {
+                return TierZeroColor;
+            }
+
+            // Tier 0
+            else if (qsr < TIER_1_RATING)
+            {
+                val = qsr / TIER_1_RATING;
+                return GetGradientColor(TierZeroColor, TierOneColor, val);
+            }
+
+            // Tier 1
+            else if (qsr < TIER_2_RATING)
+            {
+                val = qsr / (TIER_2_RATING -TIER_1_RATING);
+                return GetGradientColor(TierOneColor, TierTwoColor, val);
+            }
+
+            // Tier 2
+            else if (qsr < TIER_3_RATING)
+            {
+                val = qsr / (TIER_3_RATING - TIER_2_RATING);
+                return GetGradientColor(TierTwoColor, TierThreeColor, val);
+            }
+
+            // Tier 3
+            else if (qsr < TIER_4_RATING)
+            {
+                val = qsr / (TIER_4_RATING - TIER_3_RATING);
+                return GetGradientColor(TierThreeColor, TierFourColor, val);
+            }
+
+            // Tier 4
+            else if (qsr < MAX_TIER_RATING)
+            {
+                val = qsr / (MAX_TIER_RATING - TIER_4_RATING);
+                return GetGradientColor(TierFourColor, MaxTierColor, val);
+            }
+
+            // Max Tier
+            return MaxTierColor;
         }
 
         /// <summary>
@@ -70,6 +139,22 @@ namespace Quaver.API.Qss
         {
             var color = Color.FromArgb(255, 255, 255, 255);
             return color;
+        }
+
+        /// <summary>
+        /// Returns color from percentage in between two colors
+        /// </summary>
+        /// <param name="color1">First Color</param>
+        /// <param name="color2">Second Color</param>
+        /// <param name="value">Percentage Value (Between 0 and 1)</param>
+        /// <returns></returns>
+        public static Color GetGradientColor(Color color1, Color color2, float value)
+        {
+            var red = (int)(Math.Abs(color1.R - color2.R) * value) + Math.Min(color1.R, color2.R);
+            var green = (int)(Math.Abs(color1.G - color2.G) * value) + Math.Min(color1.G, color2.G);
+            var blue = (int)(Math.Abs(color1.B - color2.B) * value) + Math.Min(color1.B, color2.B);
+
+            return Color.FromArgb(255, red, green, blue);
         }
     }
 }
