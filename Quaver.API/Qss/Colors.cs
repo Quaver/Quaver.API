a3efe300344089b9fa57ab25b7638c4760b4ded4
staravia <denysenesouma@gmail.com>
2018-09-07 10:09:40 -0700

    Update Note Density Color

--- Quaver.API/Qss/Colors.cs
+++ Quaver.API/Qss/Colors.cs
@@ -7,62 +7,131 @@ using System.Drawing;
 
 namespace Quaver.API.Qss
 {
+    /// <summary>
+    /// Generates Colors for anything related to QSR and note density,
+    /// This class and its contents are only used for generating colors,
+    /// </summary>
     internal class Colors
     {
         /// <summary>
         /// QSR required in order to be considered first tier
         /// </summary>
-        public const float TIER_1_RATING = 10;
+        private const float TIER_1_RATING = 10;
 
         /// <summary>
         /// QSR required in order to be considered second tier
         /// </summary>
-        public const float TIER_2_RATING = 20;
+        private const float TIER_2_RATING = 20;
 
         /// <summary>
         /// QSR required in order to be considered third tier
         /// </summary>
-        public const float TIER_3_RATING = 30;
+        private const float TIER_3_RATING = 30;
 
         /// <summary>
         /// QSR required in order to be considered fourth tier
         /// </summary>
-        public const float TIER_4_RATING = 40;
+        private const float TIER_4_RATING = 40;
 
         /// <summary>
         /// QSR tiers limit
         /// </summary>
-        public const float MAX_TIER_RATING = 50;
+        private const float MAX_TIER_RATING = 50;
 
         /// <summary>
         /// Color for Beginner Tier Songs (QSR 0 - 10)
         /// </summary>
-        public static Color TierZeroColor { get; } = Color.FromArgb(128, 128, 128, 0);
+        private static Color TierZeroColor { get; } = Color.FromArgb(128, 128, 128, 0);
 
         /// <summary>
         /// Color for First Tier Songs (QSR 10 - 20)
         /// </summary>
-        public static Color TierOneColor { get; } = Color.FromArgb(255, 0, 255, 0);
+        private static Color TierOneColor { get; } = Color.FromArgb(255, 0, 255, 0);
 
         /// <summary>
         /// Color for Second Tier Songs (QSR 20 - 30)
         /// </summary>
-        public static Color TierTwoColor { get; } = Color.FromArgb(255, 255, 255, 0);
+        private static Color TierTwoColor { get; } = Color.FromArgb(255, 255, 255, 0);
 
         /// <summary>
         /// Color for Third Tier Songs (QSR 30 - 40)
         /// </summary>
-        public static Color TierThreeColor { get; } = Color.FromArgb(255, 255, 0, 0);
+        private static Color TierThreeColor { get; } = Color.FromArgb(255, 255, 0, 0);
 
         /// <summary>
         /// Color for Fourth Tier Songs (QSR 40+)
         /// </summary>
-        public static Color TierFourColor { get; } = Color.FromArgb(255, 255, 0, 255);
+        private static Color TierFourColor { get; } = Color.FromArgb(255, 255, 0, 255);
 
         /// <summary>
         /// Max Tier Color (QSR 50+)
         /// </summary>
-        public static Color MaxTierColor { get; } = Color.FromArgb(255, 255, 200, 255);
+        private static Color MaxTierColor { get; } = Color.FromArgb(255, 255, 200, 255);
+
+        /// <summary>
+        /// NPS interval used for color
+        /// </summary>
+        private const float NPS_RATING_1 = 7;
+
+        /// <summary>
+        /// NPS interval used for color
+        /// </summary>
+        private const float NPS_RATING_2 = 14;
+
+        /// <summary>
+        /// NPS interval used for color
+        /// </summary>
+        private const float NPS_RATING_3 = 21;
+
+        /// <summary>
+        /// NPS interval used for color
+        /// </summary>
+        private const float NPS_RATING_4 = 28;
+
+        /// <summary>
+        /// NPS interval used for color
+        /// </summary>
+        private const float NPS_RATING_5 = 35;
+
+        /// <summary>
+        /// NPS interval used for color
+        /// </summary>
+        private const float NPS_RATING_MAX = 42;
+
+        /// <summary>
+        /// NPS Graph Color for anything below 7nps
+        /// </summary>
+        private static Color NpsColor0 { get; } = Color.FromArgb(255, 25, 25, 204);
+
+        /// <summary>
+        /// NPS Graph Color for anything between 7-14nps
+        /// </summary>
+        private static Color NpsColor1 { get; } = Color.FromArgb(255, 51, 51, 255);
+
+        /// <summary>
+        /// NPS Graph Color for anything between 14-21nps
+        /// </summary>
+        private static Color NpsColor2 { get; } = Color.FromArgb(255, 153, 102, 255);
+
+        /// <summary>
+        /// NPS Graph Color for anything between 21-28nps
+        /// </summary>
+        private static Color NpsColor3 { get; } = Color.FromArgb(255, 255, 51, 178);
+
+        /// <summary>
+        /// NPS Graph Color for anything between 28-35nps
+        /// </summary>
+        private static Color NpsColor4 { get; } = Color.FromArgb(255, 255, 102, 51);
+
+        /// <summary>
+        /// NPS Graph Color for anything between 35-42nps
+        /// </summary>
+        private static Color NpsColor5 { get; } = Color.FromArgb(255, 255, 25, 13);
+
+        /// <summary>
+        /// NPS Graph Color for anything over 42nps
+        /// </summary>
+        private static Color NpsColorMax { get; } = Color.FromArgb(255, 178, 0, 0);
 
         /// <summary>
         /// Returns Color for general Strain Rating difficulty. Reads QSR (Quaver Strain Rating)
@@ -127,8 +196,53 @@ namespace Quaver.API.Qss
         /// <returns></returns>
         public static Color GetNoteDensityColor(float nps)
         {
-            var color = Color.FromArgb(255, 255, 255, 255);
-            return color;
+            // Gradient value for two colors
+            float val;
+
+            // Nps Interval 0 (0 - 7nps)
+            if (nps < NPS_RATING_1)
+            {
+                val = nps / NPS_RATING_1;
+                return GetGradientColor(NpsColor0, NpsColor1, val);
+            }
+
+            // Nps Interval 1 (7 - 14nps)
+            else if (nps < NPS_RATING_2)
+            {
+                val = nps / (NPS_RATING_2 - NPS_RATING_1);
+                return GetGradientColor(NpsColor1, NpsColor2, val);
+            }
+
+            // Nps Interval 2 (14 - 21nps)
+            else if (nps < NPS_RATING_3)
+            {
+                val = nps / (NPS_RATING_3 - NPS_RATING_2);
+                return GetGradientColor(NpsColor2, NpsColor3, val);
+            }
+
+            // Nps Interval 3 (21 - 28nps)
+            else if (nps < NPS_RATING_4)
+            {
+                val = nps / (NPS_RATING_4 - NPS_RATING_3);
+                return GetGradientColor(NpsColor3, NpsColor4, val);
+            }
+
+            // Nps Interval 4 (28 - 35nps)
+            else if (nps < NPS_RATING_5)
+            {
+                val = nps / (NPS_RATING_5 - NPS_RATING_4);
+                return GetGradientColor(NpsColor4, NpsColor5, val);
+            }
+
+            // Nps Interval 5 (35 - 42nps)
+            else if (nps < NPS_RATING_MAX)
+            {
+                val = nps / (NPS_RATING_MAX - NPS_RATING_5);
+                return GetGradientColor(NpsColor5, NpsColorMax, val);
+            }
+
+            // Max nps Interval (42+ nps)
+            return NpsColorMax;
         }
 
         /// <summary>
