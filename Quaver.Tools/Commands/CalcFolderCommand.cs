461a0f47143f301b2ee0faef0bb74e13593b5c92
staravia <denysenesouma@gmail.com>
2018-10-30 12:19:53 -0700

    Update CalcFolder

--- Quaver.Tools/Commands/CalcFolderCommand.cs
+++ Quaver.Tools/Commands/CalcFolderCommand.cs
@@ -37,6 +37,7 @@ namespace Quaver.Tools.Commands
         public override void Execute()
         {
             var files = Directory.GetFiles(BaseFolder, "*.qua", SearchOption.AllDirectories).ToList();
+            var output = "";
             files.AddRange(Directory.GetFiles(BaseFolder, "*.osu", SearchOption.AllDirectories));
 
             var calculatedMaps = new List<Tuple<int, string, string>>();
@@ -60,6 +61,7 @@ namespace Quaver.Tools.Commands
                     var diffCalc = map.SolveDifficulty();
 
                     Console.WriteLine($"[{i}] | {map} | {diffCalc.OverallDifficulty}");
+                    output += $"{map.Artist} - {map.Title} [{map.DifficultyName}]\t{diffCalc.OverallDifficulty}\n";
                     calculatedMaps.Add(Tuple.Create(i, map.ToString(), diffCalc.OverallDifficulty.ToString(CultureInfo.InvariantCulture)));
                 }
                 catch (Exception e)
@@ -71,7 +73,8 @@ namespace Quaver.Tools.Commands
             var table = calculatedMaps.ToStringTable(new[] { "Id", "Map", "Difficulty" }, a => a.Item1, a => a.Item2, a => a.Item3);
             Console.WriteLine(table);
 
-            File.WriteAllText($"{BaseFolder}/diff-calc.txt", table);
+            File.WriteAllText($"{BaseFolder}/diff-sheet.txt", output);
+            File.WriteAllText($"{BaseFolder}/diff-table.txt", table);
         }
     }
 }
