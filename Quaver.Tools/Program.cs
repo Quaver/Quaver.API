7f216f5d779f31f72a3bcae23271c1f9614306df
staravia <denysenesouma@gmail.com>
2018-10-30 12:16:04 -0700

    Update: CalcFolder (temp)
    made it easier for me to batch calc maps, temporary though

--- Quaver.Tools/Program.cs
+++ Quaver.Tools/Program.cs
@@ -7,6 +7,8 @@ namespace Quaver.Tools
     {
         internal static void Main(string[] args)
         {
+            args = new string[] {"-calcfolder", "c:/users/admin/desktop/qss/testmaps", "None" };
+
             if (args.Length == 0)
             {
                 Console.WriteLine($"Welcome to the Quaver.API test bench.\n" +
