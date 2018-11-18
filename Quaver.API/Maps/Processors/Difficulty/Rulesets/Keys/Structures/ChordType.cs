25cac393ac2b99ec5846923a5599357bb58910e5
staravia <denysenesouma@gmail.com>
2018-11-17 22:06:25 -0800

    Add Data Structures

---
+++ Quaver.API/Maps/Processors/Difficulty/Rulesets/Keys/Structures/ChordType.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quaver.API.Maps.Processors.Difficulty.Rulesets.Keys.Structures
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum ChordType
+    {
+        SingleTap,
+        JumpChord,
+        HandChord,
+        QuadChord,
+        NChord
+    }
+}
---
+++ Quaver.API/Maps/Processors/Difficulty/Rulesets/Keys/Structures/HandStateData.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quaver.API.Maps.Processors.Difficulty.Rulesets.Keys.Structures
+{
+    public class HandStateData
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public FingerState HandState { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ChordType ChordType { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<StrainSolverHitObject> HitObjects { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="hitObjects"></param>
+        public HandStateData(List<StrainSolverHitObject> hitObjects)
+        {
+            HitObjects = hitObjects;
+            foreach (var ob in hitObjects)
+            {
+                HandState |= ob.FingerState;
+            }
+        }
+    }
+}
