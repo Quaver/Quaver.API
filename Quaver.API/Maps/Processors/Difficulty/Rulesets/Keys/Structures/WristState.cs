94a9c08f40d42606a29499890d43dd416ed84902
staravia <denysenesouma@gmail.com>
2018-11-21 14:49:07 -0800

    Add wristState

---
+++ Quaver.API/Maps/Processors/Difficulty/Rulesets/Keys/Structures/WristState.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quaver.API.Maps.Processors.Difficulty.Rulesets.Keys.Structures
+{
+    public enum WristState
+    {
+        None,
+        AssumeUp,
+        JackUp,
+        AssumeDown,
+        JackDown
+    }
+}
