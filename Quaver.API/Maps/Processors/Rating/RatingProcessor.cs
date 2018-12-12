6c223f6f3981b930622619b965df0fb380fb292a
Swan <me@swan.moe>
2018-12-12 16:04:43 -0500

    Add license headers

--- Quaver.API/Maps/Processors/Rating/RatingProcessor.cs
+++ Quaver.API/Maps/Processors/Rating/RatingProcessor.cs
@@ -1,4 +1,11 @@
-﻿using Quaver.API.Maps.Processors.Difficulty;
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * Copyright (c) 2017-2018 Swan & The Quaver Team <support@quavergame.com>.
+*/
+
+using Quaver.API.Maps.Processors.Difficulty;
 using Quaver.API.Maps.Processors.Scoring;
 
 namespace Quaver.API.Maps.Processors.Rating
