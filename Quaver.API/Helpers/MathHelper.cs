07f3c2e57def0280bfa925c8cca67d9c050e5c6a
Swan <me@swan.moe>
2019-01-04 16:56:03 -0500

    Fix osu conversions

---
+++ Quaver.API/Helpers/MathHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Quaver.API.Helpers
+{
+    public static class MathHelper
+    {
+        public static T Clamp<T>(this T val, T min, T max) where T : IComparable<T>
+        {
+            if (val.CompareTo(min) < 0) return min;
+            return val.CompareTo(max) > 0 ? max : val;
+        }
+    }
+}
