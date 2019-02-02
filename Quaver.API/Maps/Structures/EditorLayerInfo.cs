ba944132915d1b351bcd4f67e3409b3e89df5021
Swan <me@swan.moe>
2019-02-02 12:03:35 -0500

    Add color to EditorLayerInfo

--- Quaver.API/Maps/Structures/EditorLayerInfo.cs
+++ Quaver.API/Maps/Structures/EditorLayerInfo.cs
@@ -16,5 +16,31 @@ namespace Quaver.API.Maps.Structures
         ///     Is the layer hidden in the editor?
         /// </summary>
         public bool Hidden { get; set; }
+
+        /// <summary>
+        ///     The color of the layer (default is white)
+        /// </summary>
+        public string ColorRgb { get; set; }
+
+        /// <summary>
+        ///     Converts the stringified color to a System.Drawing color
+        /// </summary>
+        /// <returns></returns>
+        public Color GetColor()
+        {
+            if (ColorRgb == null)
+                return Color.White;
+
+            var split = ColorRgb.Split(',');
+
+            try
+            {
+                return Color.FromArgb(byte.Parse(split[0]), byte.Parse(split[1]), byte.Parse(split[2]));
+            }
+            catch (Exception)
+            {
+                return Color.White;
+            }
+        }
     }
 }
