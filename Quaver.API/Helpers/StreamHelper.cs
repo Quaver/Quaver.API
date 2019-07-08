237e44bef0b8dd780875ff6e44ea7cd797557c80
Swan <me@swan.moe>
2019-07-08 08:29:06 -0400

    Make StreamHelper public

--- Quaver.API/Helpers/StreamHelper.cs
+++ Quaver.API/Helpers/StreamHelper.cs
@@ -1,7 +1,7 @@
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  * Copyright (c) 2017-2018 Swan & The Quaver Team <support@quavergame.com>.
 */
 
@@ -9,14 +9,14 @@ using System.IO;
 
 namespace Quaver.API.Helpers
 {
-    internal static class StreamHelper
+    public static class StreamHelper
     {
         /// <summary>
         ///     Turns a Stream object into a byte array
         /// </summary>
         /// <param name="input"></param>
         /// <returns></returns>
-        internal static byte[] ConvertStreamToByteArray(Stream input)
+        public static byte[] ConvertStreamToByteArray(Stream input)
         {
             using (var ms = new MemoryStream())
             {
