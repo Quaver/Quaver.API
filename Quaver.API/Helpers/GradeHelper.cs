d2f381cc4f7c51c24209fc4c21937599c700113b
Swan <me@swan.moe>
2019-09-23 09:48:14 -0400

    Add helper methods to convert Grade to level of importance

--- Quaver.API/Helpers/GradeHelper.cs
+++ Quaver.API/Helpers/GradeHelper.cs
@@ -1,7 +1,7 @@
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  * Copyright (c) 2017-2018 Swan & The Quaver Team <support@quavergame.com>.
 */
 
@@ -47,6 +47,77 @@ namespace Quaver.API.Helpers
             }
 
             return Grade.F;
+        }
+
+        /// <summary>
+        ///     <see cref="Grade"/> isn't ordered by the level of importance/lowest-highest grade.
+        ///     This returns an integer that represents the level of importance
+        /// </summary>
+        /// <param name="g"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static int GetGradeImportanceIndex(Grade g)
+        {
+            switch (g)
+            {
+                case Grade.None:
+                    return 0;
+                case Grade.F:
+                    return 1;
+                case Grade.D:
+                    return 2;
+                case Grade.C:
+                    return 3;
+                case Grade.B:
+                    return 4;
+                case Grade.A:
+                    return 5;
+                case Grade.S:
+                    return 6;
+                case Grade.SS:
+                    return 7;
+                case Grade.X:
+                    return 8;
+                case Grade.XX:
+                    return 9;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(g), g, null);
+            }
+        }
+
+        /// <summary>
+        ///     Returns the grade from the integer that represents its level of importance.
+        ///     <see cref="GetGradeImportanceIndex"/>
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public static Grade FromImportanceIndex(int i)
+        {
+            switch (i)
+            {
+                case 0:
+                    return Grade.None;
+                case 1:
+                    return Grade.F;
+                case 2:
+                    return Grade.D;
+                case 3:
+                    return Grade.C;
+                case 4:
+                    return Grade.B;
+                case 5:
+                    return Grade.A;
+                case 6:
+                    return Grade.S;
+                case 7:
+                    return Grade.SS;
+                case 8:
+                    return Grade.X;
+                case 9:
+                    return Grade.XX;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(i), i, null);
+            }
         }
     }
 }
