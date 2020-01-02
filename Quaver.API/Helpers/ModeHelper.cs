391dbcfdc5b7455e501bdf6bea4e23c992653258
Swan <me@swan.moe>
2020-01-01 21:24:08 -0500

    Add long game mode name conversion method

--- Quaver.API/Helpers/ModeHelper.cs
+++ Quaver.API/Helpers/ModeHelper.cs
@@ -1,7 +1,7 @@
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  * Copyright (c) 2017-2018 Swan & The Quaver Team <support@quavergame.com>.
 */
 
@@ -26,6 +26,25 @@ namespace Quaver.API.Helpers
                     return "4K";
                 case GameMode.Keys7:
                     return "7K";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(mode), mode, null);
+            }
+        }
+
+        /// <summary>
+        ///     Converts the game mode into the long hand version
+        /// </summary>
+        /// <param name="mode"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static string ToLongHand(GameMode mode)
+        {
+            switch (mode)
+            {
+                case GameMode.Keys4:
+                    return "4 Keys";
+                case GameMode.Keys7:
+                    return "7 Keys";
                 default:
                     throw new ArgumentOutOfRangeException(nameof(mode), mode, null);
             }
