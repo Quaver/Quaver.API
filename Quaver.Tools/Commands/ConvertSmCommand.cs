4df509e7293fea578b170e4f439c2e8385cbc538
Swan <me@swan.moe>
2020-01-16 13:01:03 -0500

    Update .sm file unit tests

--- Quaver.Tools/Commands/ConvertSmCommand.cs
+++ Quaver.Tools/Commands/ConvertSmCommand.cs
@@ -1,11 +1,11 @@
 using System.IO;
-using Quaver.API.Maps.Parsers.StepMania;
+using Quaver.API.Maps.Parsers.Stepmania;
 
 namespace Quaver.Tools.Commands
 {
     public class ConvertSmCommand : Command
     {
-        private StepmaniaConverter Converter { get; }
+        private StepFile Converter { get; }
 
         private string OutputPath { get; }
 
@@ -14,9 +14,9 @@ namespace Quaver.Tools.Commands
             OutputPath = args[2];
             Directory.CreateDirectory(OutputPath);
 
-            Converter = new StepmaniaConverter(args[1]);
+            Converter = new StepFile(args[1]);
 
-            var quas = Converter.ToQua();
+            var quas = Converter.ToQuas();
 
             for (var i = 0; i < quas.Count; i++)
                 quas[i].Save($"{OutputPath}/{i}.qua");
