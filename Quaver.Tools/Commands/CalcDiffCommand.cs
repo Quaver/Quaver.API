00f14aecbea92633cb7d6c9542026d25a0edab98
Swan <me@swan.moe>
2020-08-27 09:43:37 -0400

    Include difficulty processor version in calcdiff command output

--- Quaver.Tools/Commands/CalcDiffCommand.cs
+++ Quaver.Tools/Commands/CalcDiffCommand.cs
@@ -1,7 +1,7 @@
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  * Copyright (c) 2017-2018 Swan & The Quaver Team <support@quavergame.com>.
 */
 
@@ -12,6 +12,7 @@ using Newtonsoft.Json.Linq;
 using Quaver.API.Enums;
 using Quaver.API.Maps;
 using Quaver.API.Maps.Parsers;
+using Quaver.API.Maps.Processors.Difficulty.Rulesets.Keys;
 
 namespace Quaver.Tools.Commands
 {
@@ -64,7 +65,8 @@ namespace Quaver.Tools.Commands
                 },
                 Difficulty = new
                 {
-                    difficulty.OverallDifficulty
+                    difficulty.OverallDifficulty,
+                    DifficultyProcessorKeys.Version
                 }
             }));
         }
