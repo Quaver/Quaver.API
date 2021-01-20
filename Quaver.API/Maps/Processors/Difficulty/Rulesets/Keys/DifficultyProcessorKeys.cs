ae588a24710ded68a32a7efedc208ea3fb4b47c5
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2021-01-20 01:09:10 +0100

    Version bump diff calc

--- Quaver.API/Maps/Processors/Difficulty/Rulesets/Keys/DifficultyProcessorKeys.cs
+++ Quaver.API/Maps/Processors/Difficulty/Rulesets/Keys/DifficultyProcessorKeys.cs
@@ -26,7 +26,7 @@ namespace Quaver.API.Maps.Processors.Difficulty.Rulesets.Keys
         /// <summary>
         ///     The version of the processor.
         /// </summary>
-        public static string Version { get; } = "0.0.3";
+        public static string Version { get; } = "0.0.4";
 
         /// <summary>
         ///     Constants used for solving
