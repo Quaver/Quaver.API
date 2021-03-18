e428ca69e94f19503e7b51bd5bb2d0fc23084ef4
Swan <me@swan.moe>
2021-03-18 11:53:58 -0400

    Fix spacing

--- Quaver.API/Maps/AutoMod/Issues/Audio/AutoModIssueAudioBitrate.cs
+++ Quaver.API/Maps/AutoMod/Issues/Audio/AutoModIssueAudioBitrate.cs
@@ -3,6 +3,7 @@
     public class AutoModIssueAudioBitrate : AutoModIssue
     {
         public override AutoModIssueCategory Category { get; protected set; } = AutoModIssueCategory.Files;
+
         public AutoModIssueAudioBitrate() : base(AutoModIssueLevel.Ranking)
             => Text = $"The mp3's bitrate must be 192kbps or lower.";
     }
--- Quaver.API/Maps/AutoMod/Issues/Background/AutoModIssueNoBackground.cs
+++ Quaver.API/Maps/AutoMod/Issues/Background/AutoModIssueNoBackground.cs
@@ -3,6 +3,7 @@
     public class AutoModIssueNoBackground : AutoModIssue
     {
         public override AutoModIssueCategory Category { get; protected set; } = AutoModIssueCategory.Files;
+        
         public AutoModIssueNoBackground() : base(AutoModIssueLevel.Ranking)
             => Text = $"This map does not contain a background image.";
     }
