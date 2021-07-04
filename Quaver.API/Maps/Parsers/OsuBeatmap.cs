1c9d34d4cbaca588d783e8a98825f81f57150c0a
Kizuna <68703144+Kizuna-dev@users.noreply.github.com>
2021-07-04 22:12:53 +0200

    remove unused regex

--- Quaver.API/Maps/Parsers/OsuBeatmap.cs
+++ Quaver.API/Maps/Parsers/OsuBeatmap.cs
@@ -11,7 +11,6 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading;
 using Quaver.API.Enums;
 using Quaver.API.Helpers;
