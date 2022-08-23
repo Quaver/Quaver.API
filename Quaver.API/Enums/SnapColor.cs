6d3687abf0febdb7bc175d1ded6590f5fc0a48e2
Cuckson <31227094+Cuckson@users.noreply.github.com>
2022-08-23 21:47:24 +0100

    SnapColor enum

---
+++ Quaver.API/Enums/SnapColor.cs
@@ -0,0 +1,15 @@
+namespace Quaver.API.Enums
+{
+    public enum SnapColor
+    {
+        Red,
+        Blue,
+        Purple,
+        Yellow,
+        Pink,
+        Orange,
+        Cyan,
+        Green,
+        White
+    }
+}
