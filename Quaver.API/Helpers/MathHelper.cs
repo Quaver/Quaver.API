e557e09ac4868b9a0527c9885a8dcff91cd809ce
Swan <me@swan.moe>
2024-11-05 08:58:21 -0500

    Revert "Add scroll speed factor info for changing scroll speed multiplier in map"

--- Quaver.API/Helpers/MathHelper.cs
+++ Quaver.API/Helpers/MathHelper.cs
@@ -9,22 +9,5 @@ namespace Quaver.API.Helpers
             if (val.CompareTo(min) < 0) return min;
             return val.CompareTo(max) > 0 ? max : val;
         }
-
-        /// <summary>
-        ///     Mirrors the first <see cref="bits"/> bits of <see cref="x"/>.
-        ///     https://stackoverflow.com/a/4245986/23723435
-        /// </summary>
-        /// <param name="x"></param>
-        /// <param name="bits"></param>
-        /// <returns></returns>
-        public static uint Reverse(uint x, int bits)
-        {
-            x = ((x & 0x55555555) << 1) | ((x & 0xAAAAAAAA) >> 1); // Swap _<>_
-            x = ((x & 0x33333333) << 2) | ((x & 0xCCCCCCCC) >> 2); // Swap __<>__
-            x = ((x & 0x0F0F0F0F) << 4) | ((x & 0xF0F0F0F0) >> 4); // Swap ____<>____
-            x = ((x & 0x00FF00FF) << 8) | ((x & 0xFF00FF00) >> 8); // Swap ...
-            x = ((x & 0x0000FFFF) << 16) | ((x & 0xFFFF0000) >> 16); // Swap ...
-            return x >> (32 - bits);
-        }
     }
 }
